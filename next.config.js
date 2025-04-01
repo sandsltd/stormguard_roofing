77c54c624d7a42c58d09c10537ec07aee668cf5a
Nick Saunders <software@saunders-simmons.co.uk>
2025-04-01 13:15:11 +0100

    1

--- next.config.js
+++ next.config.js
@@ -4,7 +4,19 @@ const nextConfig = {
     ignoreDuringBuilds: true
   },
   experimental: {
-    optimizeCss: true,
+    optimizeCss: {
+      // Enable CSS optimization with critters
+      critters: {
+        // Inline critical CSS
+        preload: 'media',
+        // Don't inline fonts
+        inlineFonts: false,
+        // Reduce size
+        pruneSource: true,
+        // Reduce CSS size
+        minimize: true
+      }
+    },
     scrollRestoration: true
   }
 }
